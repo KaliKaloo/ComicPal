57c596df659a9eb9138b297854e6f51dd6f4d149
Pragya Gurung <pragyagurung002@gmail.com>
2023-02-21 03:02:36 +0000

    overview page nearly done

--- frontend-site/src/assets/style.jsx
+++ frontend-site/src/assets/style.jsx
@@ -1,9 +1,9 @@
 const styles = {
     boxWidth: "xl:max-w-[1280px] w-full",
   
-    heading2: "font-poppins font-semibold xs:text-[48px] text-[40px] text-darkGreen xs:leading-[76.8px] leading-[66.8px] w-full",
-    paragraph: "font-poppins font-normal text-dimWhite text-[17px] leading-[30.8px]",
-    paragraphDark: "font-poppins font-normal text-darkGreen text-[17px] leading-[30.8px]",
+    heading2: "font-poppins font-semibold xs:text-[20] text-[20px] pt-6 text-darkGreen xs:leading-[36.8px] leading-[56.8px] w-full",
+    paragraph: "font-poppins font-normal text-dimWhite text-[16px] leading-[30.8px]",
+    paragraphDark: "font-poppins font-normal text-darkGreen text-[14px] leading-[30.8px]",
   
     flexCenter: "flex justify-center items-center",
     flexStart: "flex justify-center items-start",
@@ -17,12 +17,12 @@ const styles = {
   };
   
   export const layout = {
-    section: `flex md:flex-row flex-col ${styles.paddingY}`,
-    sectionReverse: `flex md:flex-row flex-col-reverse ${styles.paddingY}`,
-  
+    section: `flex lg:flex-row flex-col ${styles.paddingY}`,
+    sectionReverse: `flex md:flex-row flex-col-reverse `,
+
     sectionImgReverse: `flex-1 flex ${styles.flexCenter} md:mr-10 mr-0 md:mt-0 mt-10 relative`,
     sectionImg: `flex-1 flex ${styles.flexCenter} md:ml-10 ml-0 md:mt-0 mt-10 relative`,
-  
+
     sectionInfo: `flex-1 ${styles.flexStart} flex-col`,
   };
   
--- frontend-site/src/pages/Overview/Info.jsx
+++ frontend-site/src/pages/Overview/Info.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { features } from "../../constants";
+import overviewImg1 from "../../assets/overviewImg1.png"
+import overviewImg2 from "../../assets/overviewImg2.png"
 import styles, { layout } from "../../assets/style";
 import MainButton from "../../components/ui/MainButton";
 
@@ -29,31 +31,38 @@ function FeatureCard({ icon, title, content, index }) {
 
 function Info() {
   return (
-    <section id="features" className={layout.section}>
-      <div className={layout.sectionInfo}>
-        <h2 className={styles.heading2}>
-        Your Wingman,
-          <br className="sm:block hidden" /> for Creative Leadership!
-        </h2>
-        <p className={`${styles.paragraphDark} max-w-[470px] mt-5`}>
-          AI technology has revolutionized the creative space, but some artists
-          still view it as a taboo. Fear not, because ComicPal is not here to
-          replace your creative vision. Instead, think of it as a virtual
-          assistant to elevate your artistry to new heights. With just the right
-          amount of AI assistance, ComicPal is here to help unleash your
-          creativity and enhance your productivity, making your creative journey
-          even more exciting and fulfilling. Join the AI revolution with
-          ComicPal and unlock your true artistic potential!
-        </p>
+    // <div className="flex flex-row">
+    <section className="grid lg:grid-cols-2 gap-4">
+      <section className={layout.sectionReverse}>
+          <div className={layout.sectionImgReverse}>
+            <img src={overviewImg1} alt="overview img 1" className="w-[100%] h-[100%] relative z-[5]"/>
+          </div>
+          
+          <div className={`${layout.sectionInfo} px-10`}>
+            <h2 className={styles.heading2}>
+            Your Wingman,
+              <br className="sm:block hidden" /> for Creative Leadership!
+            </h2>
+            <p className={`${styles.paragraphDark} max-w-[350px]`}>
+              AI technology has revolutionized the creative space, but some artists
+              still view it as a taboo. Fear not, because ComicPal is not here to
+              replace your creative vision. Instead, think of it as a virtual
+              assistant to elevate your artistry to new heights. With just the right
+              amount of AI assistance, ComicPal is here to help unleash your
+              creativity and enhance your productivity!
+            </p>
 
-        <MainButton styles="mt-10 bg-blue-gradient" text="Get Started" />
-      </div>
-
-      <div className={`${layout.sectionImg} flex-col`}>
-        {features.map((feature, index) => (
-          <FeatureCard key={feature.id} {...feature} index={index} />
-        ))}
-      </div>
+            <MainButton styles="mt-10 bg-blue-gradient" text="Get Started" />
+          </div>
+        {/* </div> */}
+        {/* <div className={`${layout.sectionImg} flex-col`}>
+            {features.map((feature, index) => (
+              <FeatureCard key={feature.id} {...feature} index={index} />
+            ))}*/}
+      </section>
+      <div className={layout.sectionImgReverse}>
+          <img src={overviewImg2} alt="overview img 2" className="w-[100%] h-[100%] relative z-[5]" />
+        </div>
     </section>
   );
 }
