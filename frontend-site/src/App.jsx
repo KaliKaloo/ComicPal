94e7dafaf9b5507cb7414f7cb2326ef76fabc7f0
Pragya Gurung <pragyagurung002@gmail.com>
2023-03-10 16:46:45 +0000

    udpate vercel.json

--- frontend-site/src/App.jsx
+++ frontend-site/src/App.jsx
@@ -3,6 +3,13 @@ import "./assets/normal.css";
 import OverviewPage from "./pages/Overview/OverviewPage.jsx";
 
 function App() {
+	const [buildId] = useBuildId(); // useSWR under the hood
+	const prevBuildId = usePrevious(buildId);
+	React.useEffect(() => {
+		if (prevBuildId && buildId && prevBuildId !== buildId) {
+			Router.reload();
+		}
+	}, [buildId, prevBuildId]);
 	return (
 		<div>
 			<OverviewPage />
